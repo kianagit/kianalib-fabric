658d9105b11ef2d161cec713f0af3bca5c1c3834
BuildTools <unconfigured@null.spigotmc.org>
2023-12-06 09:49:27 +0100

    ...

---
+++ src/main/java/de/chloedev/kianalibfabric/io/FileConfiguration.java
@@ -0,0 +1,37 @@
+package de.chloedev.kianalibfabric.io;
+
+import java.io.File;
+import java.io.IOException;
+import org.simpleyaml.configuration.file.YamlFile;
+
+
+public class FileConfiguration {
+
+    private YamlFile file;
+
+    public FileConfiguration(File file) {
+        this.file = new YamlFile(file);
+        try {
+            this.file.createOrLoad();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T read(Class<T> type, String key, T def) {
+        return (T) this.file.get(key, def);
+    }
+
+    public FileConfiguration write(String key, Object value, boolean saveToFileNow) {
+        this.file.set(key, value);
+        if (saveToFileNow) {
+            try {
+                this.file.save();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return this;
+    }
+}
