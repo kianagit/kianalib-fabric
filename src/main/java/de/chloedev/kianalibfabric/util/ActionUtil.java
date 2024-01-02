9270888ed2fbe68d870a6e27bf681d2a4b54d916
kianagit <kiana@chloedev.de>
2024-01-02 07:51:39 +0100

    ActionUtil: - Add method "get"   - Basically the same as using a Supplier normally, but easier to access for stuff like method call arguments

--- src/main/java/de/chloedev/kianalibfabric/util/ActionUtil.java
+++ src/main/java/de/chloedev/kianalibfabric/util/ActionUtil.java
@@ -1,7 +1,17 @@
 package de.chloedev.kianalibfabric.util;
 
+import java.util.function.Supplier;
+
 public class ActionUtil {
 
-    public static void doNothing(Object... values){
+    public static void doNothing(Object... values) {
+    }
+
+    public static <T> T get(Supplier<T> supplier) {
+        return supplier.get();
+    }
+
+    public interface Task {
+        void run();
     }
 }
